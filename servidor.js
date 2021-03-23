a50a691c04951ac854489d03caa382b890905e18
Kevin Ramos Dos Santos De Oliveira <kevram52@outlook.com>
2021-03-23 06:33:08 -0300

    Corrigindo bando de dados 2

--- servidor.js
+++ servidor.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 const bancoDeDados = knex ({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
       ssl: true
     }
 });
